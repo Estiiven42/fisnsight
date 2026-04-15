8b50b90a5c00b984cbde967c75b0aa39149554c4
Estiiven42 <estiiven42@gmail.com>
2026-04-15 14:30:48 -0500

    feat: servicio autenticacion funcionando con endpoint /test

---
+++ servicio-autenticacion/src/main/java/com/finsight/autenticacion/controladores/TestController.java
@@ -0,0 +1,13 @@
+package com.finsight.autenticacion.controladores;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Servicio autenticacion funcionando";
+    }
+}
